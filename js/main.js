c2f16255720b2877de242363453dba540bff522c
JohnyBlacksad <126941830+JohnyBlacksad@users.noreply.github.com>
2023-09-25 15:01:16 +0500

    First Commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+const btnDarkMode = document.querySelector(".dark-mode__btn");
+
+if (window.matchMedia && window.matchMedia ("(prefers-color-scheme: dark)").matches ) {
+    btnDarkMode.classList.add("dark-mode__btn--active");
+    document.body.classList.add("dark");
+}
+
+
+if (localStorage.getItem('darkMode') === 'dark') {
+    btnDarkMode.classList.add("dark-mode__btn--active");
+    document.body.classList.add("dark");
+}
+
+else if (localStorage.getItem('darkMode') === 'light') {
+    btnDarkMode.classList.remove("dark-mode__btn--active");
+    document.body.classList.remove("dark");
+}
+
+
+
+window.matchMedia && window.matchMedia ("(prefers-color-scheme: dark)").addEventListener('change', (event) => {
+    const newColorScheme = event.matches ? "dark" : 'light';
+
+    if (newColorScheme === 'dark') {
+        btnDarkMode.classList.add("dark-mode__btn--active");
+        document.body.classList.add("dark");
+        localStorage.setItem('darkMode', 'dark');
+    }
+    else {
+        btnDarkMode.classList.remove("dark-mode__btn--active");
+        document.body.classList.remove("dark");
+        localStorage.setItem('darkMode', 'light');
+    }
+});
+
+
+btnDarkMode.onclick = function () {
+    btnDarkMode.classList.toggle("dark-mode__btn--active");
+    const isDark = document.body.classList.toggle('dark')
+
+    if (isDark) {
+        localStorage.setItem('darkMode', 'dark')
+    } 
+    else {
+        localStorage.setItem('darkMode', 'light')
+    }
+};
